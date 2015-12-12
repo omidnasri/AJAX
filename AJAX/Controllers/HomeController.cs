eea2e5c4723e7dff2b7e223b1bc9523741b3d15c
Omid Nasri <omid.nasri@outlook.com>
2015-12-12 21:37:25 +0330

    12-12-2015

---
+++ AJAX/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace AJAX.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string GetDateTime()
+        {
+            return DateTime.Now.ToString();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string GetName(string yourname)
+        {
+            return yourname;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public string GetYear()
+        {
+            return DateTime.Now.Year.ToString();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public string GetYear(string year)
+        {
+            return year;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Json()
+        {
+            return Json(new { name = "omid", family = "nasri" });
+        }
+    }
+}
---
+++ AJAX/Scripts/NLibrary.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function() {
+    $("[datetime-now]").click(function() {
+        var model = "/Home/GetDateTime";
+        $.get(model, {yourname:"omid nasri"}, function(data) {
+            alert(data);
+        });
+    });
+    $("[yourname-now]").click(function () {
+        var model = "/Home/GetName";
+        $.get(model, {yourname:"omid nasri"}, function (data) {
+            alert(data);
+        });
+    });
+    $("[getyear-now]").click(function () {
+        var model = "/Home/GetYear";
+        $.post(model, function (data) {
+            alert(data);
+        });
+    });
+    $("[getyear]").click(function () {
+        var model = "/Home/GetYear";
+        $.post(model, {year:"2015"}, function (data) {
+            alert(data);
+        });
+    });
+    $("[return-json]").click(function () {
+        var model = "/Home/Json";
+        $.post(model, function (data) {
+            alert(data.name);
+            alert(data.family);
+        });
+    });
+});
